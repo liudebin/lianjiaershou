3f950d42da8c280d49e6e9e99dfaf06159030ca2
liuguobin <guobin.liu@holaverse.com>
2019-01-09 10:16:55 +0800

    'community'

--- lianjia/spiders/lianjiaCommunity.py
+++ lianjia/spiders/lianjiaCommunity.py
@@ -33,7 +33,7 @@ class ToScrapeSpiderXPath(scrapy.Spider):
     cursor = dbObject.cursor()
     cursor.execute("USE test")
     # sql = "INSERT INTO articles(author,title,times,url,admire,likes) VALUES(%s,%s,%s,%s,%s,%s)"
-    sql = "select distinct community_link link from rent_detail_lianjia where community_code is not null ;"
+    sql = "select distinct community_link link from ershoufang_detail_lianjia where community_code is not null ;"
     cursor.execute(sql)
     results = cursor.fetchall()
     box = []
@@ -42,22 +42,60 @@ class ToScrapeSpiderXPath(scrapy.Spider):
     start_urls = box
 
     def parse(self, response):
-        print response
-        for quote in response.xpath('//div[@class="content__list--item"]'):
-            # print quote
-            price = int(quote.xpath('.//span[@class="content__list--item-price"]/em/text()')[0].extract())
+        for quote in response.xpath('//li [@class="clear LOGCLICKDATA"]'):
+            title = getList(quote.xpath('.//div[@class="info clear"]/div[@class="title"]/a/text()'))
+            link = getList(quote.xpath('.//a[@class="noresultRecommend img "]/@href'))
+            house_code = getList(quote.xpath('.//a[@class="noresultRecommend img "]/@data-housecode'))
+            community_code = getList(quote.xpath(
+                './/div[@class="info clear"]/div[@class="priceInfo"]/div[@class="unitPrice"]/@data-rid'))
+
+            community_link = getList(
+                quote.xpath('.//div[@class="info clear"]/div[@class="address"]/div[@class="houseInfo"]/a/@href'))
+            community = getList(
+                quote.xpath('.//div[@class="info clear"]/div[@class="address"]/div[@class="houseInfo"]/a/text()'))
+
+            town_link = getList(
+                quote.xpath('.//div[@class="info clear"]/div[@class="flood"]/div[@class="positionInfo"]/a/@href'))
+            town_disc = getList(
+                quote.xpath('.//div[@class="info clear"]/div[@class="flood"]/div[@class="positionInfo"]/a/text()'))
+
+            nn = quote.xpath('.//div[@class="info clear"]/div[@class="address"]/div[@class="houseInfo"]')
+            house_info = nn[0].xpath('string(.)')[0].extract().replace("\n", "").replace("  ", "")
+            chan_quan = quote.xpath('.//div[@class="info clear"]/div[@class="flood"]/div[@class="positionInfo"]')[0] \
+                            .xpath('string(.)')[0].extract().replace("\n", "").replace("  ", ""),
+
+            follow_info = quote.xpath('.//div[@class="info clear"]/div[@class="followInfo"]')[0].xpath(
+                'string(.)')[0].extract() \
+                .replace("\n", "").replace("  ", "")
+            tags = quote.xpath('.//div[@class="info clear"]/div[@class="tag"]')[0].xpath('string(.)')[0].extract() \
+                .replace("\n", "").replace("  ", "")
+            price = int(
+                quote.xpath('.//div[@class="info clear"]/div[@class="priceInfo"]/div[@class="totalPrice"]/span/text()')[
+                    0].extract())
+            price_content = quote.xpath('.//div[@class="info clear"]/div[@class="priceInfo"]/div[@class="totalPrice"]')[
+                0] \
+                .xpath('string(.)')[0].extract().replace("\n", "").replace("  ", "")
+            unit_price = getList(
+                quote.xpath('.//div[@class="info clear"]/div[@class="priceInfo"]/div[@class="unitPrice"]/span/text()'))
+
             yield {
-                'title': getList(quote.xpath('.//div/p[@class="content__list--item--title twoline"]/a/text()')),
-                'link': baseUrl + getList(quote.xpath('.//div/p[@class="content__list--item--title twoline"]/a/@href')),
-                'house_code': getList(quote.xpath('.//div/p[@class="content__list--item--title twoline"]/a/@href'))[8:-5],
-                'area_link': baseUrl + getList(quote.xpath('.//div/p[@class="content__list--item--des"]/a[position()=1]/@href')),
-                'area_disc': getList(
-                    quote.xpath('.//div/p[@class="content__list--item--des"]/a[position()=1]/text()')),
-                'town_link': baseUrl + getList(
-                    quote.xpath('.//div/p[@class="content__list--item--des"]/a[position()=2]/@href')),
-                'town_disc': getList(
-                    quote.xpath('.//div/p[@class="content__list--item--des"]/a[position()=2]/text()')),
-                'price': price
+                'title': title,
+                'link': link,
+                'house_code': house_code,
+                'community_code': community_code,
+
+                'community_link': community_link,
+                'community': community,
+                'town_link': town_link,
+                'town_disc': town_disc,
+                'house_info': house_info,
+                'chan_quan':
+                    chan_quan,
+                'follow_info': follow_info,
+                'tags': tags,
+                'price': price,
+                'price_content': price_content,
+                'unit_price': unit_price
             }
 
             # next_page_url = response.css("li.next > a::attr(href)").extract_first()
